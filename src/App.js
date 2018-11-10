bdb8f9ade67c18d74d13ab2312dd78cf075289f5
tzq668766 <tzq618766@gmail.com>
2018-11-10 22:28:10 +0800

    资源引用修改为相对路径

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from './components/Home';
 import News from './components/News';
