b4d0c470683f53efa1a91753f7860b9a24532a71
tzq668766 <tzq618766@gmail.com>
2018-11-10 23:31:50 +0800

    更新about页面

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,36 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import {Grid, Row, Button} from 'react-bootstrap';
+import {Grid, Row, Button, Jumbotron,Image,Col} from 'react-bootstrap';
+import person from '../assets/person.png';
+import banner from '../assets/banner.jpg';
 import './About.css';
 
 class About extends Component {
     render() {
         return (
             <Grid>
-                <Row>
-                    <h2>关于我们</h2>
+                <img src={banner} alt="" className="banner"/>
+                <Jumbotron>
+                    <h2>我是关于页面</h2>
+                    <p>我是内容，我是内容，我是内容，我是内容，我是内容，我是内容，</p>
+                </Jumbotron>
+                <Row className="show-grid text-center">
+                    <Col xs={12} sm={6} md={4} sm={2}>
+                        <Image src={person} className="profile-img" circle />
+                        <h3>我是标题</h3>
+                        <p>我是简介................</p>
+                    </Col>
+                    <Col xs={12} sm={6} md={4} sm={2}>
+                        <Image src={person} className="profile-img" circle />
+                        <h3>我是标题</h3>
+                        <p>我是简介................</p>
+                    </Col>
+                    <Col xs={12} sm={6} md={4} sm={2}>
+                        <Image src={person} className="profile-img" circle />
+                        <h3>我是标题</h3>
+                        <p>我是简介................</p>
+                    </Col>
                 </Row>
-                <Link to='/home'>
-                    <Button className="btn btn-success">首页</Button>
-                </Link>
             </Grid>
         );
     }
