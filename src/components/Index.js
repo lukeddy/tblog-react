ff7158a782af1acd92d7357a9085fb6ca0632df3
tzq668766 <tzq618766@gmail.com>
2019-01-01 23:50:17 +0800

    调整进度条位置

--- src/components/Index.js
+++ src/components/Index.js
@@ -73,16 +73,17 @@ class Index extends Component {
     }
 
     render() {
-        const {pager,catList,currentFilter, alertData}=this.state
+        const {pager,catList,currentFilter, alertData,loading}=this.state
         return (
             <div className="container main">
                 <Alert alertData={alertData}/>
                 <div className="col-md-9">
+                    <BarLoader loading={loading} widthUnit={'px'} heightUnit={'px'} width={823} height={6} color={'#fa0000'}/>
                     <div className="panel">
                         <div className="header">
                             {catList &&<Menu catList={catList} currentTab={currentFilter.tab} goToTab={this.goToTab}/>}
                         </div>
-                        <Suspense fallback={<BarLoader widthUnit={'px'} heightUnit={'px'} width={823} height={6} color={'#fa0000'}/>}>
+                        <Suspense fallback={<div className={'text-center'}>加载帖子数据...</div>}>
                         <div className="inner no-padding">
                             {pager&&pager.content.length===0 && <div className='row text-center'>没有数据</div>}
                             {pager&&pager.content.length>0 && <PostList postList={pager.content}></PostList>}
