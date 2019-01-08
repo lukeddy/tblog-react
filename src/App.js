d2612148daa6a9dad9b1080ef1f8676210a17d23
tzq668766 <tzq618766@gmail.com>
2019-01-08 22:10:35 +0800

    添加nprogresss示例，这个示例以后的next.js项目可以参考

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
 import Index from './components/Index';
 import About from './components/About';
 import Nav from './components/Nav'
@@ -14,6 +14,7 @@ import CategoryEdit from "./components/admin/CategoryEdit";
 import Post from "./components/admin/Post";
 import PostAdd from "./components/admin/PostAdd";
 import PostEdit from "./components/admin/PostEdit";
+import MyRoute from "./route/MyRoute";
 
 
 class App extends Component {
@@ -23,20 +24,19 @@ class App extends Component {
             <div>
                 <Nav></Nav>
                 <Switch>
-                    <Route exact path="/" component={Index}/>
-                    {/*<Redirect from="/" to="/home"></Redirect>*/}
-                    <Route path="/home" component={Index}/>
-                    <Route path="/about" component={About}/>
-                    <Route path="/login" component={Login}/>
-                    <Route path="/register" component={Register}/>
-                    <Route path="/detail/:id" component={PostDetail}/>
-                    <Route path="/category" component={Category}/>
-                    <Route path="/categoryadd" component={CategoryAdd}/>
-                    <Route path="/categoryedit/:id" component={CategoryEdit}/>
-                    <Route path="/post" component={Post}/>
-                    <Route path="/postadd" component={PostAdd}/>
-                    <Route path="/postedit/:id" component={PostEdit}/>
-                    <Route component={NoMatch}/>
+                    <MyRoute exact path="/" component={Index}/>
+                    <MyRoute path="/home" component={Index}/>
+                    <MyRoute path="/about" component={About}/>
+                    <MyRoute path="/login" component={Login}/>
+                    <MyRoute path="/register" component={Register}/>
+                    <MyRoute path="/detail/:id" component={PostDetail}/>
+                    <MyRoute path="/category" component={Category}/>
+                    <MyRoute path="/categoryadd" component={CategoryAdd}/>
+                    <MyRoute path="/categoryedit/:id" component={CategoryEdit}/>
+                    <MyRoute path="/post" component={Post}/>
+                    <MyRoute path="/postadd" component={PostAdd}/>
+                    <MyRoute path="/postedit/:id" component={PostEdit}/>
+                    <MyRoute component={NoMatch}/>
                 </Switch>
                 <Footer></Footer>
             </div>
